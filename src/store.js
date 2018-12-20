6d7d539f63a853af7c6a839c44adbc38c30c20e5
Krystian Pietroń <krystianpietr@gmail.com>
2018-12-20 10:29:45 +0100

    add store, component-> Users.js

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+const Users = props => (
+    <div>
+
+    </div>
+)
+
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({
+    
+})
+
+export default connect()(Users)
